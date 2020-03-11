f9e26d17edbebf26ee37a741d40f040087875c8e
Ardika Bagus S <me@ardikabs.id>
2020-03-11 11:35:20 +0700

    Renaming fzf.zsh

--- zsh/zshrc.d/999.fzf.zsh
+++ zsh/zshrc.d/999.fzf.zsh
@@ -11,13 +11,7 @@ export FZF_DEFAULT_OPTS="--no-mouse --height 50% -1
     --preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file ||
         (bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -300'
     --preview-window='right:hidden:wrap'
-    --bind='f3:execute(bat --style=numbers {} || less -f {}),
-            f2:toggle-preview,
-            ctrl-d:half-page-down,
-            ctrl-u:half-page-up,
-            ctrl-a:select-all+accept,
-            ctrl-y:execute-silent(echo {+} | xclip -sel c -i),
-            ctrl-x:execute(rm -i {+})+abort'"
+    --bind='f3:execute(bat --style=numbers {} || less -f {}),f2:toggle-preview,ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-a:select-all+accept,ctrl-y:execute-silent(echo {+} | xclip -sel c -i),ctrl-x:execute(rm -i {+})+abort'"
 
 export FZF_DEFAULT_COMMAND="git ls-files --cached --others --exclude-standard || fd --type f --type l $FD_OPTIONS"
 export FZF_CTRL_T_COMMAND="fd $FD_OPTIONS"
