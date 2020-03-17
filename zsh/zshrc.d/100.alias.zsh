32b668408f7bbc77038e67c4a278016de1f36d20
Ardika Bagus S <me@ardikabs.id>
2020-03-17 15:09:34 +0700

    Minor changes on tmux.conf and add some alias

--- zsh/zshrc.d/100.alias.zsh
+++ zsh/zshrc.d/100.alias.zsh
@@ -8,6 +8,6 @@ alias ssno='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 
 alias t=tmux
 alias ta="t a -t"
-alias tn="t new -t"
+alias tn="t new -s"
 alias tls="t ls"
 alias tks="t kill-session -t"
