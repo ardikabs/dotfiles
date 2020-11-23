2d97ec7df65f7c097c598b8b8b252bbe0cb84d59
Ardika Bagus <ardikabs@gmail.com>
2020-11-23 15:15:54 +0700

    Put all togather for cloud-related command line

--- zsh/zshrc.d/999.cloud.zsh
+++ zsh/zshrc.d/999.cloud.zsh
@@ -1,6 +1,9 @@
 #!/usr/bin/sh
 # -------------------------------------------
-# Google Cloud
+# Cloud-related command line
+# Currently:
+# 1. Google Cloud Platform
+# 2. Amazon Web Services
 # -------------------------------------------
 
 # The next line updates PATH for the Google Cloud SDK.
@@ -11,4 +14,8 @@ fi
 # The next line enables shell command completion for gcloud.
 if [ -f /usr/local/google-cloud-sdk/completion.zsh.inc ]; then
 	source /usr/local/google-cloud-sdk/completion.zsh.inc
+fi
+
+if type aws_completer >/dev/null 2>&1; then
+	complete -C aws_completer aws
 fi
