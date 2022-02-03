7b2cdaa52832f4ed71c8061ca46d990751363673
Ardika Bagus <me@ardikabs.com>
2022-02-04 00:15:30 +0700

    test: test ci

--- zsh/zshrc.d/101.custom.sh
+++ zsh/zshrc.d/101.custom.sh
@@ -33,3 +33,17 @@ ipsec_vpn_cleanup() {
   echo >&2 "cleaning ipsec vpn session..."
   docker rm -f "$(docker ps -aq --filter label=app=ipsec)" >/dev/null 2>&1
 }
+
+ssh_tunnel() {
+  [ -f /tmp/jumpbox.pid ] && kill -15 "$(cat /tmp/jumpbox.pid 2>/dev/null)" 2>/dev/null
+  if ssh -q -fN -L6443:172.18.1.2:6443 -L8443:172.18.2.3:6443 jumpbox; then
+    pgrep -f 'ssh.*jumpbox' > /tmp/jumpbox.pid
+  fi
+
+  if nc -zv 127.0.0.1 6443 >/dev/null 2>&1 \
+    && nc -zv 127.0.0.1 8443 >/dev/null 2>&1; then
+    echo >&2 'message: successfully connected to the jumpbox network'
+  else
+    echo >&2 'error: failed to connect to the jumpbox network'
+  fi
+}
