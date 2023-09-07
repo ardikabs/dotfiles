e3987597bee354cfdf27661e1c9c060c37a14e0b
Ardika Bagus <me@ardikabs.com>
2023-09-07 20:29:53 +0700

    feat: add another aliases, and modify mode

--- zsh/zshrc.d/010.alias.sh
+++ zsh/zshrc.d/010.alias.sh
@@ -11,3 +11,27 @@ alias ta="t a -t"
 alias tn="t new -s"
 alias tls="t ls"
 alias tks="t kill-session -t"
+
+alias less='less -F'
+alias grep='grep --color'
+alias ls='ls -hF --color=tty'
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
+alias tree='tree -C'
+
+alias ..='cd ..'
+alias .1='cd ..'
+alias ...='cd ../..'
+alias .2='cd ../..'
+alias ....='cd ../../..'
+alias .3='cd ../../..'
+alias .....='cd ../../../..'
+alias .4='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .5='cd ../../../../..'
+alias .......='cd ../../../../../..'
+alias .6='cd ../../../../../..'
+alias .7='cd ../../../../../../..'
+alias .8='cd ../../../../../../../..'
+alias .9='cd ../../../../../../../../..'
--- zsh/zshrc.d/300.colima.sh
+++ zsh/zshrc.d/300.colima.sh
@@ -8,7 +8,7 @@ if [[ "$(uname)" == "Darwin" ]]; then
   export DOCKER_HOST=unix://$HOME/.colima/default/docker.sock
 
   # Load colima completion for zsh
-  if type kubectl >/dev/null 2>&1; then
+  if type colima >/dev/null 2>&1; then
     # shellcheck disable=SC1090
     source <(colima completion zsh)
   fi
