49f5225c97f71cdc45644958e6a951298d4aa961
Ardika Bagus <me@ardikabs.com>
2024-09-02 16:32:32 +0700

    chore: rewriting history
    - change default gituser and add additional PATH on zsh
    - set 600 to kubeconfig
    - add uname check to export PATH
    - add aws_eni func
    
    Signed-off-by: Ardika Bagus <me@ardikabs.com>

--- zsh/zshrc.d/505.aws.sh
+++ zsh/zshrc.d/505.aws.sh
@@ -9,3 +9,15 @@ export AWS_REGION=ap-southeast-1
 if type aws_completer >/dev/null 2>&1; then
   complete -C aws_completer aws
 fi
+
+aws_eni() {
+  TYPE=$1
+
+  aws ec2 describe-instance-types \
+    --filters "Name=instance-type,Values=$TYPE.*" \
+    --query "InstanceTypes[].{ \
+        Type: InstanceType, \
+        MaxENI: NetworkInfo.MaximumNetworkInterfaces, \
+        IPv4addrPerENI: NetworkInfo.Ipv4AddressesPerInterface}" \
+    --output table
+}
